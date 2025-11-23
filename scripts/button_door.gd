e1bf8e593e0a4eaaedfe29cfa41ff0bc9fb7d6b1
Aya <aya.pk.contact@gmail.com>
2025-11-23 13:28:16 +0000

    Fix block/door bug

--- scripts/button_door.gd
+++ scripts/button_door.gd
@@ -3,8 +3,10 @@ class_name ButtonDoor extends ButtonTarget
 @export var speed: float = 50.0
 
 var start_y: float
+var start_x: float
 
 func _ready() -> void:
+	start_x = global_position.x
 	pass
 
 func _process(_delta: float) -> void:
@@ -14,11 +16,14 @@ func _process(_delta: float) -> void:
 		velocity.y = -speed
 	else:
 		velocity.y = speed
-
+	if global_position.x != start_x:
+		global_position.x = start_x
+	
 	if global_position.y <= start_y:
 		move_and_slide()
 	if global_position.y > start_y and start_y:
 		global_position.y = start_y
+	
 
 func active() -> void:
 	is_active = true
